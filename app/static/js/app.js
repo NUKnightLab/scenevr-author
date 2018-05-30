2056902619b5cab2168155fc64bd608e3e15b87b
David Latimore <dgl@u.northwestern.edu>
2018-05-29 21:55:03 -0500

    initial css setup with styled jsx, initial login screen work

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,49 @@
 import React from "react";
+
 export default class App extends React.Component {
+
+	constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  signin() {
+    console.log("Google sign in activated");
+  }
+
   render () {
-    return <p> Hello React!</p>;
+    return (
+      <div id="login">
+        <h1> SceneVR </h1>;
+        <h3> Greeting or some other introductory copy? </h3>
+        <div id="login-button" onClick={this.signin}> </div>
+
+        <style jsx> {` 
+          #login {
+            height: 100vh;
+            width: 100%;
+            text-align: center;
+            font-family: "Avenir Next";
+          }
+
+          #login h1 {
+            font-weight: bold;
+            color: #999;
+          }
+
+          #login h3 {
+            font-style: italic;
+            color: #999;
+          }
+
+          #login-button {
+            width: 80%;
+            height: 60px;
+            margin: 0 auto;
+            background-color: #ff675e;
+          }
+        `}</style>
+      </div>
+    ); 
   }
 }
