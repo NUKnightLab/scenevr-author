b5046e1466fb2a150e1b85b501c4dfbee6eb787a
David Latimore <dgl@u.northwestern.edu>
2018-06-06 15:40:59 -0500

    added project styles back in for landing page

--- app/static/js/projects.jsx
+++ app/static/js/projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Redirect } from 'react-router';
+import styles from '../stylesheets/projects.css';
 
 import IndividualProject from './components/IndividualProject.jsx';
 
