bfee6d86cc44ef006375fba7c5f5ecd022594c9d
Joe Germuska <joe@germuska.com>
2018-07-18 19:30:14 -0500

    testing/fixing boto storage implementation

--- app/server/settings.py
+++ app/server/settings.py
@@ -5,7 +5,8 @@ from os.path import abspath, dirname, join
 from os import environ as env
 import sys
 
-TEST_MODE = env.get('TEST_MODE') == 'True'
+USE_LOCAL_STORAGE = env.get('TEST_MODE') == True or env.get(
+    'USE_LOCAL_STORAGE') == 'True'
 
 CORE_ROOT = dirname(abspath(__file__))
 PROJECT_ROOT = dirname(CORE_ROOT)
@@ -21,7 +22,7 @@ try:
     AWS_STORAGE_BUCKET_NAME = env['AWS_STORAGE_BUCKET_NAME']
     AWS_STORAGE_KEY_PREFIX = env['AWS_STORAGE_KEY_PREFIX']
 except KeyError as e:
-    if not TEST_MODE:
+    if not USE_LOCAL_STORAGE:
         print("Missing environment variable {}".format(e))
         sys.exit(-1)
 
