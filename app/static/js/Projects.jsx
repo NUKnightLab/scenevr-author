062b312bfe691a103b0b12f92f1459388cd97d2b
Zach Wise <zach@digitalartwork.net>
2018-07-20 09:33:22 -0500

    Tweak language

--- app/static/js/Projects.jsx
+++ app/static/js/Projects.jsx
@@ -101,11 +101,11 @@ export default class Projects extends React.Component {
 
                     <div id="header">
                         <div id="logout">
-                            <a href="/logout">Log out</a>
+                            <a href="/logout">Sign out</a>
                         </div>
                         <div id="logo">Scene <span>VR</span></div>
                         <div id="user">
-                            <img id="user-picture" description={this.state.userName} src={this.state.userPicture}/>
+                            <img id="user-picture" alt={this.state.userName} src={this.state.userPicture}/>
                         </div>
                     </div>
 
